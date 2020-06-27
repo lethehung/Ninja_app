a4d0a88661de1f731f992e09bf0b8896f27f3de7
lethehung <okkaka12345@gmail.com>
2020-06-27 11:57:19 +0700

    a

--- app/Http/Controllers/API/AttendanceController.php
+++ app/Http/Controllers/API/AttendanceController.php
@@ -140,6 +140,7 @@ class AttendanceController extends Controller
     }
     public function createFolder(Request $request){
         $image= array();
+        if(!file_exists('Images/'.$request->id.'/train'))
         mkdir('Images/'.$request->id.'/train', 0777, true);
         foreach ($_FILES as $k => $val){
            if(!empty($val["tmp_name"])){
